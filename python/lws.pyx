0c95beac33142f0d272fee6389ee3df4140590f9
Jonathan Le Roux <leroux@merl.com>
2017-05-06 00:40:17 -0400

    python module compiles, added Makefile. now only need to test it...

--- python/lws.pyx
+++ python/lws.pyx
@@ -1,11 +1,16 @@
-#cimport lws_functions
 cimport numpy as np
 import numpy as np
 
-cdef extern from "c/lws_functions.h":
+cdef extern from "lwslib/lws_functions.h":
     void LWSQ2(double *Sr, double *Si, double *wr, double *wi, int *w_flag, double *AmpSpec, int Nreal, int M, int L, double threshold)
     void LWSQ4(double *Sr, double *Si, double *wr, double *wi, int *w_flag, double *AmpSpec, int Nreal, int M, int L, double threshold)
     void LWSanyQ(double *Sr, double *Si, double *wr, double *wi, int *w_flag, double *AmpSpec, int Nreal, int M, int L, int Q, double threshold)
+    void NoFuture_LWSQ2(double *Sr, double *Si, double *wr, double *wi, int *w_flag, double *AmpSpec, int Nreal, int M, int L, double threshold)
+    void NoFuture_LWSQ4(double *Sr, double *Si, double *wr, double *wi, int *w_flag, double *AmpSpec, int Nreal, int M, int L, double threshold)
+    void NoFuture_LWSanyQ(double *Sr, double *Si, double *wr, double *wi, int *w_flag, double *AmpSpec, int Nreal, int M, int L, int Q, double threshold)
+    void TF_RTISI_LA(double *Sr, double *Si, double *wr, double *wi, 
+        double *wr_asym_init, double *wi_asym_init, double *wr_asym_full, double *wi_asym_full, int *w_flag, int *w_flag_ai, int *w_flag_af,  
+        double *AmpSpec, int iter, int LA, int Nreal, int M, int L, int Q, double *ThresholdArray, int update)
 
 def extspec(S, L, Q):
     Nreal,T = S.shape
@@ -63,6 +68,113 @@ def lws(np.ndarray[np.complex128_t, ndim=2] S,
         else:
             LWSanyQ(&ExtSr[0,0], &ExtSi[0,0], &Wr[0,0,0], &Wi[0,0,0], &Wflag[0,0,0], &AmpSpec[0,0], Nreal, T, L,Q, threshold)
     
+    # Extract the non-redundant part of the spectrogram
+    S_out =  ExtSr[L:(Nreal+L),(Q-1):(Q-1+T)] + 1j * ExtSi[L:(Nreal+L),(Q-1):(Q-1+T)]
+    
+    return S_out
+
+def nofuture_lws(np.ndarray[np.complex128_t, ndim=2] S, 
+        np.ndarray[np.complex128_t, ndim=3] W, 
+        np.ndarray[np.double_t, ndim=1] thresholds):
+        
+        
+    S          = np.ascontiguousarray(S)
+    W          = np.ascontiguousarray(W)
+    #thresholds = np.ascontiguousarray(thresholds)
+    
+    L = W.shape[0] - 1
+    Q = W.shape[1]
+    iterations = len(thresholds)
+    Nreal = S.shape[0]
+    T = S.shape[1]
+    if Nreal % 2 == 0:
+        raise ValueError('Please only include non-negative frequencies in the input spectrogram.')
+    N = 2*(Nreal-1)
+    
+    cdef np.ndarray[np.double_t, ndim=3, mode="c"] Wr = np.ascontiguousarray(W.real)
+    cdef np.ndarray[np.double_t, ndim=3, mode="c"] Wi = np.ascontiguousarray(W.imag)
+    
+    # Get a boolean mask specifying which weights to use or skip
+    w_threshold = 1.0e-12
+    cdef np.ndarray[int, ndim=3, mode="c"] Wflag = np.ascontiguousarray(np.abs(W) > w_threshold, dtype=np.dtype("i"))
+    
+    # Extend the spectrogram to avoid having to deal with values outside the boundaries
+    ExtS = extspec(S, L , Q)
+    cdef np.ndarray[np.double_t, ndim=2, mode="c"] ExtSr = np.ascontiguousarray(ExtS.real)
+    cdef np.ndarray[np.double_t, ndim=2, mode="c"] ExtSi = np.ascontiguousarray(ExtS.imag)
+    # Store the amplitude spectrogram
+    cdef np.ndarray[np.double_t, ndim=2, mode="c"] AmpSpec = np.ascontiguousarray(np.abs(ExtS))
+    mean_amp = np.mean(np.abs(S))
+    
+    # Perform the phase updates
+    for i in range(iterations):
+        threshold = thresholds[i] * mean_amp
+        if Q == 2:
+            NoFuture_LWSQ2(&ExtSr[0,0], &ExtSi[0,0], &Wr[0,0,0], &Wi[0,0,0], &Wflag[0,0,0], &AmpSpec[0,0], Nreal, T, L, threshold)
+        elif Q == 4:
+            NoFuture_LWSQ4(&ExtSr[0,0], &ExtSi[0,0], &Wr[0,0,0], &Wi[0,0,0], &Wflag[0,0,0], &AmpSpec[0,0], Nreal, T, L, threshold)
+        else:
+            NoFuture_LWSanyQ(&ExtSr[0,0], &ExtSi[0,0], &Wr[0,0,0], &Wi[0,0,0], &Wflag[0,0,0], &AmpSpec[0,0], Nreal, T, L,Q, threshold)
+    
+    # Extract the non-redundant part of the spectrogram
+    S_out =  ExtSr[L:(Nreal+L),(Q-1):(Q-1+T)] + 1j * ExtSi[L:(Nreal+L),(Q-1):(Q-1+T)]
+    
+    return S_out
+
+
+def online_lws(np.ndarray[np.complex128_t, ndim=2] S, 
+        np.ndarray[np.complex128_t, ndim=3] W, 
+        np.ndarray[np.complex128_t, ndim=3] W_asym_init, 
+        np.ndarray[np.complex128_t, ndim=3] W_asym_full, 
+        np.ndarray[np.double_t, ndim=1] thresholds,
+	int LA):
+        
+        
+    S          = np.ascontiguousarray(S)
+    W          = np.ascontiguousarray(W)
+    W_ai       = np.ascontiguousarray(W_asym_init)
+    W_af       = np.ascontiguousarray(W_asym_full)
+    
+    L = W.shape[0] - 1
+    Q = W.shape[1]
+    iterations = len(thresholds)
+    Nreal = S.shape[0]
+    T = S.shape[1]
+    if Nreal % 2 == 0:
+        raise ValueError('Please only include non-negative frequencies in the input spectrogram.')
+    N = 2*(Nreal-1)
+    
+    cdef np.ndarray[np.double_t, ndim=3, mode="c"] Wr = np.ascontiguousarray(W.real)
+    cdef np.ndarray[np.double_t, ndim=3, mode="c"] Wi = np.ascontiguousarray(W.imag)
+    cdef np.ndarray[np.double_t, ndim=3, mode="c"] Wr_ai = np.ascontiguousarray(W_ai.real)
+    cdef np.ndarray[np.double_t, ndim=3, mode="c"] Wi_ai = np.ascontiguousarray(W_ai.imag)
+    cdef np.ndarray[np.double_t, ndim=3, mode="c"] Wr_af = np.ascontiguousarray(W_af.real)
+    cdef np.ndarray[np.double_t, ndim=3, mode="c"] Wi_af = np.ascontiguousarray(W_af.imag)
+    
+    # Get a boolean mask specifying which weights to use or skip
+    w_threshold = 1.0e-12
+    cdef np.ndarray[int, ndim=3, mode="c"] Wflag = np.ascontiguousarray(np.abs(W) > w_threshold, dtype=np.dtype("i"))
+    cdef np.ndarray[int, ndim=3, mode="c"] Wflag_ai = np.ascontiguousarray(np.abs(W_ai) > w_threshold, dtype=np.dtype("i"))
+    cdef np.ndarray[int, ndim=3, mode="c"] Wflag_af = np.ascontiguousarray(np.abs(W_af) > w_threshold, dtype=np.dtype("i"))
+    
+    # Extend the spectrogram to avoid having to deal with values outside the boundaries
+    ExtS = extspec(S, L , Q)
+    cdef np.ndarray[np.double_t, ndim=2, mode="c"] ExtSr = np.ascontiguousarray(ExtS.real)
+    cdef np.ndarray[np.double_t, ndim=2, mode="c"] ExtSi = np.ascontiguousarray(ExtS.imag)
+    # Store the amplitude spectrogram
+    cdef np.ndarray[np.double_t, ndim=2, mode="c"] AmpSpec = np.ascontiguousarray(np.abs(ExtS))
+    mean_amp = np.mean(np.abs(S))
+    thresholds = np.ascontiguousarray(thresholds * mean_amp)
+
+    update_type = 2
+    # Perform the phase updates
+    TF_RTISI_LA(&ExtSr[0,0], &ExtSi[0,0], &Wr[0,0,0], &Wi[0,0,0],
+                &Wr_ai[0,0,0], &Wi_ai[0,0,0], &Wr_af[0,0,0], &Wi_af[0,0,0],
+                &Wflag[0,0,0], &Wflag_ai[0,0,0], &Wflag_af[0,0,0],
+                &AmpSpec[0,0],
+                iterations, LA, Nreal, T, L, Q, 
+                &thresholds[0], update_type);
+
     # Extract the non-redundant part of the spectrogram
     S_out =  ExtSr[L:(Nreal+L),(Q-1):(Q-1+T)] + 1j * ExtSi[L:(Nreal+L),(Q-1):(Q-1+T)]
     
--- python/setup.py
+++ python/setup.py
@@ -1,17 +1,46 @@
 from setuptools import setup, Extension
 import numpy as np
+from codecs import open
+import os
+
+# This file largely taken from https://github.com/Martinsos/edlib/blob/a77e81678abd9392f1e13ec8585831721a1f354a/bindings/python/setup.py
+
+# Build directly from cython source file(s) if user wants so (probably for some experiments).
+# Otherwise, pre-generated c source file(s) are used.
+# User has to set environment variable LWS_USE_CYTHON.
+# e.g.: LWS_USE_CYTHON=1 python setup.py install
+cmdclass = {}
+USE_CYTHON = os.getenv('LWS_USE_CYTHON', False)
+if USE_CYTHON:
+    from Cython.Build import build_ext
+    edlib_module_src = "lws.pyx"
+    cmdclass['build_ext'] = build_ext
+else:
+    edlib_module_src = "lws.bycython.c"
+
+
+long_description = ""
+# Load README into long description.
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     # Information
     name = "lws",
+    description = "Fast spectrogram phase reconstruction using Local Weighted Sums",
+    long_description = long_description,
     version = "1.0.0",
-    url = "",
+    url = "http://www.jonathanleroux.org/",
     author = "Jonathan Le Roux",
+    author_email = "leroux@merl.com",
     license = "Apache 2.0",
     keywords = "phase reconstruction stft",
     # Build instructions
     ext_modules = [Extension("lws",
-                             sources=["c/lws_functions.cpp","lws.pyx"],
-                             include_dirs=["c/",np.get_include()],
-                             language="c++")]
+                             sources=["lws.bycython.cpp","lwslib/lws_functions.cpp"],
+                             include_dirs=["lwslib/",np.get_include()],
+                             language="c++",
+                             extra_compile_args=["-O3"])],
+    cmdclass = cmdclass
 )
